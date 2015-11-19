b92696532b7da8929070814a961f0d2657f880ef
Antonio Piccolboni <antonio@piccolboni.info>
2015-11-19 13:15:47 -0800

    something funky happening with colnames, reported upstream. This makes tests go through for now

--- pkg/tests/window-functions.R
+++ pkg/tests/window-functions.R
@@ -29,22 +29,22 @@ batting = {
   else
     copy_to_from_local(my_db, Batting, "batting")}
 batting = select(batting, playerID, yearID, teamID, G, AB:H)
-batting = arrange(batting, playerID, yearID, teamID)
-players = group_by(batting, playerID)
+batting = arrange(batting, playerid, yearid, teamid) #lowercased cols, documented bug
+players = group_by(batting, playerid)
 cache(batting)
 cache(players)
 # For each player, find the two years with most hits
-filter(players, min_rank(desc(H)) <= 2 & H > 0)
+filter(players, min_rank(desc(h)) <= 2 & h > 0)
 # Within each player, rank each year by the number of games played
-mutate(players, g_rank = min_rank(G))
+mutate(players, g_rank = min_rank(g))
 
 # For each player, find every year that was better than the previous year
-filter(players, G > lag(G))
+filter(players, g > lag(g))
 # For each player, compute avg change in games played per year
-mutate(players, g_change = (G - lag(G)) / (yearID - lag(yearID)))
+mutate(players, g_change = (g - lag(g)) / (yearid - lag(yearid)))
 
 # For each player, find all where they played more games than average
-filter(players, G > mean(G))
+filter(players, g > mean(g))
 # For each, player compute a z score based on number of games played
-mutate(players, g_z = (G - mean(G)) / sd(G))
+mutate(players, g_z = (g - mean(g)) / sd(g))
 
