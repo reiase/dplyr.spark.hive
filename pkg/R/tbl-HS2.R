311a29382ce9d6f479e4239754b1429df8f63237
Antonio Piccolboni <antonio@piccolboni.info>
2016-02-16 17:45:47 -0800

    fix for #7

--- pkg/R/tbl-HS2.R
+++ pkg/R/tbl-HS2.R
@@ -67,10 +67,7 @@ mutate_.tbl_HS2 =
     .data$mutate = TRUE
     no.dup.select = .data$select[!as.character(.data$select) %in% names(input)]
     new = update(.data, select = c(no.dup.select, input))
-    if (dplyr:::uses_window_fun(input, .data)) {
-      collapse(new) }
-    else {
-      new}}
+    collapse(new) }
 
 #modeled after filter_ methods in http://github.com/hadley/dplyr,
 #under MIT license
